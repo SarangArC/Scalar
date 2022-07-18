80311d368bc3bfa38f072c0ab07b0668654f233e
sarangarc <109346576+SarangArC@users.noreply.github.com>
2022-07-18 20:19:58 +0530

    Update MeasureActivity.kt

--- app/src/main/java/com/hl3hl3/arcoremeasure/MeasureActivity.kt
+++ app/src/main/java/com/hl3hl3/arcoremeasure/MeasureActivity.kt
@@ -22,16 +22,16 @@ class MeasureActivity : AppCompatActivity() {
 
     private var displayRotationHelper: DisplayRotationHelper? = null
 
-    // Tap handling and UI.
+    
     private val QUEUED_SIZE= 16
     private val queuedSingleTaps = ArrayBlockingQueue<MotionEvent>(QUEUED_SIZE)
     private val queuedScrollDx = ArrayBlockingQueue<Float>(QUEUED_SIZE)
     private val queuedScrollDy = ArrayBlockingQueue<Float>(QUEUED_SIZE)
     private val gestureDetectorListener = object : SimpleOnGestureListener() {
         override fun onSingleTapUp(e: MotionEvent): Boolean {
-            // Queue tap if there is space. Tap is lost if queue is full.
+            
             queuedSingleTaps.offer(e)
-            //            log(TAG, "onSingleTapUp, e=" + e.getRawX() + ", " + e.getRawY());
+            
             return true
         }
 
@@ -43,7 +43,7 @@ class MeasureActivity : AppCompatActivity() {
             e1: MotionEvent, e2: MotionEvent,
             distanceX: Float, distanceY: Float
         ): Boolean {
-//            log(TAG, "onScroll, dx=" + distanceX + " dy=" + distanceY);
+
             queuedScrollDx.offer(distanceX)
             queuedScrollDy.offer(distanceY)
             return true
@@ -74,24 +74,24 @@ class MeasureActivity : AppCompatActivity() {
             viewWidth: Int,
             viewHeight: Int
         ) {
-            // draw cube & line from last frame
+            
             if (anchors.size < 1) {
-                // no point
+                
                 showResult("")
             } else {
-                // draw selected cube
+                
                 renderer.drawSelectedCube(anchors[currentSelected])
 
                 val sb = StringBuilder()
                 var total = 0.0
                 var point1: Pose
 
-                // draw first cube
+                
                 var point0 = anchors[0].pose
                 val lastTap = queuedSingleTaps.peek()
                 drawCube(0, lastTap, renderer)
 
-                // draw the rest cube
+                
                 for (i in 1 until anchors.size) {
                     point1 = anchors[i].pose
 
@@ -118,28 +118,26 @@ class MeasureActivity : AppCompatActivity() {
             }
 
 
-            // check if there is any touch event
+            
             queuedSingleTaps.poll()?.let { tap ->
                 if (camera.trackingState == TrackingState.TRACKING) {
                     for (hit in frame.hitTest(tap)) {
-                        // Check if any plane was hit, and if it was hit inside the plane polygon.j
+                        
                         val trackable = hit.trackable
-                        // Creates an anchor if a plane or an oriented point was hit.
+                        
                         if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)
                             || (trackable is Point
                                     && trackable.orientationMode
                                     == Point.OrientationMode.ESTIMATED_SURFACE_NORMAL)
                         ) {
-                            // Cap the number of objects created. This avoids overloading both the
-                            // rendering system and ARCore.
+                            
+                            
                             if (anchors.size >= 16) {
                                 anchors[0].detach()
                                 anchors.removeAt(0)
                             }
 
-                            // Adding an Anchor tells ARCore that it should track this position in
-                            // space. This anchor will be used in PlaneAttachment to place the 3d model
-                            // in the correct position relative both to the world and to the plane.
+                      
                             anchors.add(hit.createAnchor())
                             break
                         }
@@ -187,7 +185,7 @@ class MeasureActivity : AppCompatActivity() {
         super.onWindowFocusChanged(hasFocus)
         Logger.logStatus("onWindowFocusChanged()")
         if (hasFocus) {
-            // Standard Android full-screen functionality.
+            
             window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -201,13 +199,13 @@ class MeasureActivity : AppCompatActivity() {
     private fun initiate() {
         val rotationHelper = DisplayRotationHelper(this)
 
-        // session
+        
         val arcoreSession = Session(this)
         val config = Config(arcoreSession)
         arcoreSession.configure(config)
         session = arcoreSession
 
-        // renderer & surfaceview
+        
         if (gestureDetector == null) {
             glSerfaceRenderer =
                 GLSurfaceRenderer(this, arcoreSession, rotationHelper, renderListener)
@@ -221,7 +219,7 @@ class MeasureActivity : AppCompatActivity() {
 
                 preserveEGLContextOnPause = true
                 setEGLContextClientVersion(2)
-                setEGLConfigChooser(8, 8, 8, 8, 16, 0) // Alpha used for plane blending.
+                setEGLConfigChooser(8, 8, 8, 8, 16, 0) 
                 setRenderer(glSerfaceRenderer)
                 renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
             }
